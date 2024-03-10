b9b210a4291619f8dfb85f1b15ba5a89cc799e1b
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-03-10 17:01:28 +0530

    created the user and video model

---
+++ src/models/user.model.ts
@@ -0,0 +1,95 @@
+import mongoose, { Schema, Types } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+//error facing in index:true in username
+/*
+the problem is that it is taking all type as array 
+ in username
+*/
+interface Iuser extends Document {
+  username: string;
+  email: string;
+  fullName: string;
+  avatar: string;
+  coverImage?: string;
+  password: string;
+  refreshToken: string;
+  watchHistory: Types.ObjectId;
+}
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: [true, "username is required"],
+      unique: [true, "username already exist "],
+      lowercase: [true, "username Should be in lower case"],
+      trim: [true, ""],
+      index: [true],
+    },
+    email: {
+      type: String,
+      require: [true, "email is required"],
+      unique: [true, "email already exist "],
+    },
+    fullName: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    avatar: {
+      type: String, //cloudiary
+      require: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      require: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+//for encryption of the password  middleware just before saving the database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //temp fix by if see other sol
+  //   if (this.password == null || this.password == undefined) return next();
+  if (this.password) bcrypt.hash(this.password, 10);
+
+  next();
+});
+//it is to check the password provide is correct or not
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessTokens = function () {
+  const secret: string = process.env.ACCESS_TOKEN_SECRET as string;
+  return jwt.sign(
+    { _id: this._id, username: this.username, email: this.email },
+    secret,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshTokens = function () {
+  const secret: string = process.env.REFRESH_TOKEN_SECRET as string;
+  return jwt.sign({ _id: this._id }, secret, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
+
+export const User = mongoose.model<Iuser>("User", userSchema);
---
+++ src/models/vide.model.ts
@@ -0,0 +1,52 @@
+import mongoose, { Schema, Types } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+interface Ivideo extends Document {
+  videoFile: string;
+  thumbnail: string;
+  owner: Types.ObjectId;
+  title: string;
+  description: string;
+  duration: number;
+  views: number;
+  isPublished: boolean;
+}
+
+const VideoSchema = new Schema({
+  videoFile: {
+    type: String,
+    require: true,
+  },
+  thumbnail: {
+    type: String,
+    require: true,
+  },
+  owner: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    require: [true, "Title is Required "],
+  },
+  description: {
+    type: String,
+    require: [true, "Description is Required "],
+  },
+  duration: {
+    type: Number,
+    require: true,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  isPublished: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+VideoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model<Ivideo>("Video", VideoSchema);
